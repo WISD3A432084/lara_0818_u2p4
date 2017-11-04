01f0a50e6085b2ee6a00e4c4c9c015db1c7e2731
Xin <lovemarks19@gmail.com>
2017-11-04 20:04:49 +0800

    設定 navigation 及 index 視圖上的 <a 標籤，確認所有 href 設定都更改為對應的 route 名稱

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,7 +24,7 @@
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <div class="post-preview">
-                    <a href="post.html">
+                    <a href="{{route('posts.index')}}">
                         <h2 class="post-title">
                             Man must explore, and this is exploration at its greatest
                         </h2>
@@ -36,7 +36,7 @@
                 </div>
                 <hr>
                 <div class="post-preview">
-                    <a href="post.html">
+                    <a href="{{route('posts.index')}}">
                         <h2 class="post-title">
                             I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
                         </h2>
@@ -45,7 +45,7 @@
                 </div>
                 <hr>
                 <div class="post-preview">
-                    <a href="post.html">
+                    <a href="{{route('posts.index')}}">
                         <h2 class="post-title">
                             Science has not yet mastered prophecy
                         </h2>
@@ -57,7 +57,7 @@
                 </div>
                 <hr>
                 <div class="post-preview">
-                    <a href="post.html">
+                    <a href="{{route('posts.index')}}">
                         <h2 class="post-title">
                             Failure is not an option
                         </h2>
--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -7,23 +7,23 @@
                 <span class="sr-only">Toggle navigation</span>
                 Menu <i class="fa fa-bars"></i>
             </button>
-            <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+            <a class="navbar-brand" href="{{route('posts.index')}}">Start Bootstrap</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
                 <li>
-                    <a href="index.html">Home</a>
+                    <a href="{{route('posts.index')}}">Home</a>
                 </li>
                 <li>
-                    <a href="about.html">About</a>
+                    <a href="{{route('posts.about')}}">About</a>
                 </li>
                 <li>
-                    <a href="post.html">Sample Post</a>
+                    <a href="{{route('posts.show')}}">Sample Post</a>
                 </li>
                 <li>
-                    <a href="contact.html">Contact</a>
+                    <a href="{{route('posts.contact')}}">Contact</a>
                 </li>
             </ul>
         </div>

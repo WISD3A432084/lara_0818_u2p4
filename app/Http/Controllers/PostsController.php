2af20e3f7a659cd5a792dc49ed8742e2aa823733
Xin <lovemarks19@gmail.com>
2017-11-04 19:53:09 +0800

    設定 Controller 回傳對應的 view

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,16 +8,16 @@ class PostsController extends Controller
 {
     //
     public function index(){
-        //
+        return view('index');
     }
     public function show(){
-        //
+        return view('post');
     }
     public function about(){
-    //
+        return view('about');
     }
     public function contact(){
-        //
+        return view('contact');
     }
 
 }
